5f5ed4b12324bc88b941538246ea3084273dac35
yash ganorkar <yash.ganorkar@outlook.com>
2018-10-01 09:59:55 -0700

    Project Setup

---
+++ pixel-city/pixel-city/Controller/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  MapVC.swift
+//  pixel-city
+//
+//  Created by Yash Ganorkar on 10/1/18.
+//  Copyright © 2018 Yash Ganorkar. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
