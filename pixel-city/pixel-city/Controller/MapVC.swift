51be2ddfb1dbcad803c357ab58dab494d3ff781d
yash ganorkar <yash.ganorkar@outlook.com>
2018-10-01 11:51:29 -0700

    Implemented functionality to use location services to determine user's location.

--- pixel-city/pixel-city/Controller/MapVC.swift
+++ pixel-city/pixel-city/Controller/MapVC.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
 
     
     @IBOutlet weak var mapView: MKMapView!
     
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius : Double = 1000
+    
     @IBAction func centerMapButtonWasPressed(_ sender: Any) {
+        
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
+        locationManager.delegate = self
+        
+        configureLocationServices()
     }
 
 
@@ -27,5 +39,29 @@ class MapVC: UIViewController {
 
 extension MapVC : MKMapViewDelegate {
     
+    //zoom in onto user location if the coordinates are available
+    
+    func centerMapOnUserLocation(){
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegion.init(center: coordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
+        
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate{
+    //get location permission from user
+    func configureLocationServices(){
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    
+    //center on user's location
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
 }
 
