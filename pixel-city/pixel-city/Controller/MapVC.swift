7536955e3e94a58d8f6bc0b00a8736f21cccb2f4
yash ganorkar <yash.ganorkar@outlook.com>
2018-10-01 13:16:33 -0700

    Implemented swipe-able ui view to show the images downloaded from Flickr which are geo-tagged for that particular location; Added activity indicator to show the progress of downloading images from the internet; Added UICollectionView programmatically which would contain all the images downloaded from the api.

--- pixel-city/pixel-city/Controller/MapVC.swift
+++ pixel-city/pixel-city/Controller/MapVC.swift
@@ -11,13 +11,23 @@ import MapKit
 import CoreLocation
 
 class MapVC: UIViewController,UIGestureRecognizerDelegate {
-
     
+
+    @IBOutlet weak var pullUpViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var pullUpView: UIView!
     @IBOutlet weak var mapView: MKMapView!
     
     var locationManager = CLLocationManager()
     let authorizationStatus = CLLocationManager.authorizationStatus()
     let regionRadius : Double = 1000
+    
+    var screenSize = UIScreen.main.bounds
+    
+    var spinner : UIActivityIndicatorView?
+    var progressLabel : UILabel?
+    
+    var flowLayout = UICollectionViewFlowLayout()
+    var collectionView : UICollectionView?
     
     @IBAction func centerMapButtonWasPressed(_ sender: Any) {
         
@@ -33,6 +43,14 @@ class MapVC: UIViewController,UIGestureRecognizerDelegate {
         
         configureLocationServices()
         addDoubleTap()
+        
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: flowLayout)
+        collectionView?.register(PhotoCell.self, forCellWithReuseIdentifier: "photoCell")
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        collectionView?.backgroundColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
+        
+        pullUpView.addSubview(collectionView!)
     }
     
     func addDoubleTap() {
@@ -42,6 +60,60 @@ class MapVC: UIViewController,UIGestureRecognizerDelegate {
         doubleTap.delegate = self
         
         mapView.addGestureRecognizer(doubleTap)
+    }
+    
+    func addSwipe() {
+        let swipe = UISwipeGestureRecognizer(target: self, action: #selector(animateViewDown))
+        
+        swipe.direction = .down
+        
+        pullUpView.addGestureRecognizer(swipe)
+    }
+    
+    @objc func animateViewDown() {
+        pullUpViewHeightConstraint.constant = 0
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func addSpinner() {
+        spinner = UIActivityIndicatorView()
+        spinner?.center = CGPoint(x: (screenSize.width / 2) - ((spinner?.frame.width)! / 2), y: 150)
+        spinner?.style = .whiteLarge
+        spinner?.color = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        spinner?.startAnimating()
+        
+        collectionView?.addSubview(spinner!)
+    }
+    
+    func removeSpinner(){
+        if spinner != nil {
+            spinner?.removeFromSuperview()
+        }
+    }
+    
+    func addProgressLabel() {
+        progressLabel = UILabel()
+        progressLabel?.frame = CGRect(x: (screenSize.width / 2) - 100, y: 175, width: 200, height: 40)
+        progressLabel?.font = UIFont(name: "Avenir Next", size: 18)
+        progressLabel?.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        progressLabel?.textAlignment = .center
+        progressLabel?.text = "Loading..."
+        collectionView?.addSubview(progressLabel!)
+    }
+    
+    func removeProgressLabel(){
+        if progressLabel != nil {
+            progressLabel?.removeFromSuperview()
+        }
+    }
+    
+    func animateViewUp() {
+        pullUpViewHeightConstraint.constant = 300
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
     }
 }
 
@@ -58,6 +130,14 @@ extension MapVC : MKMapViewDelegate {
     
     @objc func dropPin(sender : UITapGestureRecognizer) {
         removePin()
+        removeSpinner()
+        removeProgressLabel()
+        
+        animateViewUp()
+        
+        addSwipe()
+        addSpinner()
+        addProgressLabel()
         
         let touchPoint = sender.location(in: mapView)
         let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
@@ -69,6 +149,20 @@ extension MapVC : MKMapViewDelegate {
         let coordinateRegion = MKCoordinateRegion(center: touchCoordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
         
         mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        let pinAnnotation = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "droppablePin")
+        
+        pinAnnotation.pinTintColor = UIColor.orange
+        pinAnnotation.animatesDrop = true
+        
+        return pinAnnotation
     }
     
     func removePin() {
@@ -91,6 +185,21 @@ extension MapVC: CLLocationManagerDelegate{
     //center on user's location
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         centerMapOnUserLocation()
+    }
+}
+
+extension MapVC: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as? PhotoCell
+        return cell!
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
 }
 
---
+++ pixel-city/pixel-city/Views/PhotoCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoCell.swift
+//  pixel-city
+//
+//  Created by Yash Ganorkar on 10/1/18.
+//  Copyright © 2018 Yash Ganorkar. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    override init(frame: CGRect){
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
